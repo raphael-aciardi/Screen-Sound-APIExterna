dd9600dc1be01ce49806c30cfd633c81d24204f7
raphael-aciardi <996193248ph@gmail.com>
2024-07-08 20:50:18 -0300

    First Class Using Json

---
+++ Screen-Sound-API/Modelos/Music.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace Screen_Sound_API.Modelos;
+
+internal class Music
+{
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+}
---
+++ Screen-Sound-API/Program.cs
@@ -0,0 +1,12 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex) 
+    {
+        Console.WriteLine($"We have a problem: {ex.Message}");
+    }
+}
